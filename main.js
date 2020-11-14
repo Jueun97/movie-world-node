6bf5008ce2389c9075cbc9e71e5007ae21337568
Jueun <zxnm0901@naver.com>
2020-11-14 17:12:04 +0900

    js: modal

--- main.js
+++ main.js
@@ -6,4 +6,20 @@ window.addEventListener('scroll', (e) => {
         navbar.classList.add('active');
     else
         navbar.classList.remove('active');
-})
+});
+const moreInfoBtn = document.querySelector('.infoBtn');
+moreInfoBtn.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    modal.style.display = 'block'; 
+    const body = document.querySelector('.body')
+    body.classList.add('not-scroll');
+});
+
+const cancleBtn = document.querySelector('.modal__cancle');
+cancleBtn.addEventListener('click', () => {
+    console.log('hihi');
+    const modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+    const body = document.querySelector('.body');
+    body.classList.remove('not-scroll');
+});
