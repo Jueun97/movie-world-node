2fcfa2d3d695d6dc1751b65d4da2aa2e53033d78
Jueun <zxnm0901@naver.com>
2020-11-16 23:36:20 +0900

    js: modal for each movies

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//change navbar backgroundColor when scrolling down
 window.addEventListener('scroll', (e) => {
     scrollHeight = window.scrollY;
     const navbar = document.querySelector('#navbar');
@@ -7,22 +8,39 @@ window.addEventListener('scroll', (e) => {
     else
         navbar.classList.remove('active');
 });
+
+//show up modal when 'more info' button is clicked
 const moreInfoBtn = document.querySelector('.infoBtn');
 moreInfoBtn.addEventListener('click', () => {
-    const modal = document.querySelector('.modal');
-    modal.style.display = 'block'; 
-    const container = document.querySelector('.bodyContainer');
-    container.classList.add('invisible');
-    container.parentElement.style.overflow = 'hidden';
+    openModal();
 });
 
+//hide modal when 'x' button in modal is clicked
 const cancleBtn = document.querySelector('.modal__cancle');
 cancleBtn.addEventListener('click', () => {
-    console.log('hihi');
-    const modal = document.querySelector('.modal');
+    closeModal();
+});
+
+//show up modal when 'movie icon' in movie poster is clicked
+const movies = document.querySelectorAll('.movie-icon');
+movies.forEach(movie => {
+    movie.addEventListener('click', (e) => {
+        openModal();
+    });
+})
+
+// show up modal
+const modal = document.querySelector('.modal');
+const container = document.querySelector('.bodyContainer');
+function openModal() {
+    modal.style.display = 'block'; 
+    container.classList.add('invisible');
+    container.parentElement.style.overflow = 'hidden';
+};
+
+// hide modal
+function closeModal() {
     modal.style.display = 'none';
- 
-    const container = document.querySelector('.bodyContainer');
     container.classList.remove('invisible');
     container.parentElement.style.overflow = 'initial';
-});
+};
