75a9945f06bdec9acdedb43a6909d96a8023612e
Jueun <zxnm0901@naver.com>
2020-12-16 19:29:28 +0900

    add friends video

--- video.js
+++ video.js
@@ -3,27 +3,34 @@ const urlParams = new URLSearchParams(window.location.search);
 const MOVIE_ID = urlParams.get('id');
 const videoVideo = document.querySelector('.video-video-src');
 const videoIframe = document.querySelector('.video-iframe');
+const movieID = MOVIE_ID.toLowerCase().replace(/(\s*)/g, "");
+
+const cancleBtn = document.querySelector('.video__cancle');
+
+cancleBtn.addEventListener('click', () => {
+    window.history.back();
+});
 
 if (MOVIE_ID == "" || MOVIE_ID == 'undefined') {
     alert('동영상을 재셍할 수 없습니다. 죄송합니다.');
     //window.location.href = './index.html';
     window.history.back();
 }
-else if (MOVIE_ID == "friends") {
+else if (movieID.indexOf("friends") >= 0) {
+    videoVideo.src = `./data/videos/${MOVIE_ID}.mp4`; 
     videoVideo.parentNode.style.display = "block";
     videoIframe.style.display = "none";
-    videoVideo.src = "./data/videos/main.mp4"; 
+    cancleBtn.classList.add('videoFrame');
 }
 else {
     videoVideo.parentNode.style.display = "none";
     videoIframe.style.display = "block";
+    cancleBtn.classList.remove('videoFrame');
     videoIframe.src = `https://www.youtube.com/embed/${MOVIE_ID}`; 
 }
 
-const cancleBtn = document.querySelector('.video__cancle');
-cancleBtn.addEventListener('click', () => {
-    window.history.back();
-});
+
+
 
 
    
